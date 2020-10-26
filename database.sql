2f175d3ad2758d4a50bca7d4703afc8d75297769
Dustin Cote <dustincote@hotmail.com>
2020-10-26 14:15:29 -0500

    post route for c/api/animal/cow tested with postman and working

--- database.sql
+++ database.sql
@@ -8,3 +8,37 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "pastures" (
+	"pasture_id" serial PRIMARY KEY,
+	"pasture_name" varchar(255) NOT NULL UNIQUE,
+	"user_id" integer REFERENCES "user"
+);
+
+CREATE TABLE "notes" (
+	"note_id" serial PRIMARY KEY,
+	"note" TEXT NOT NULL,
+	"animal_id" integer REFERENCES "animals",
+	"date" TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE "animals" (
+	"animal_id" serial PRIMARY KEY,
+	"dam_id" varchar(255),
+	"sire_id" varchar(255),
+	"tag_number" varchar(255) NOT NULL,
+	"gender" varchar(75) NOT NULL,
+	"user_id" integer NOT NULL,
+	"cull" BOOLEAN NOT NULL DEFAULT 'false',
+	"archived" BOOLEAN NOT NULL DEFAULT 'false',
+	"date_archived" DATE,
+	"birth_date" DATE,
+	"disposition" integer,
+	"status" varchar(255) NOT NULL DEFAULT 'alive',
+	"calving_ease" integer,
+	"castrated" BOOLEAN DEFAULT 'false',
+	"birthweight" integer,
+	"calf" BOOLEAN NOT NULL,
+	"close_to_calving" BOOLEAN NOT NULL DEFAULT 'false'
+	
+);
---
+++ server/routes/animal.router.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const { rejectUnauthenticated,} = require('../modules/authentication-middleware');
+
+/**
+ * GET route template
+ */
+router.get('/', rejectUnauthenticated, (req, res) => {
+  let queryText =`SELECT * FROM "animals";`
+  pool.query(queryText).then(result => res.send(result.rows))
+  .catch(e => {
+    console.log('error getting animals', e);
+    res.sendStatus(500);
+  })
+});
+
+/**
+ * POST route template
+ */
+//post to /api/animal/cow this is used to post a new cow to the database
+router.post('/cow', rejectUnauthenticated, (req, res) => {
+  // console.log(req.body);
+  console.log(req.user);
+  console.log('adding a cow with tag number', req.body.tag_number);
+  let body = req.body;
+  let values = [body.dam_id,
+  body.sire_id,
+  body.tag_number,
+  body.gender,
+  req.user.id,
+  body.birth_date,
+  body.disposition,
+  body.calving_ease,
+  body.castrated,
+  body.birthweight,
+  body.calf,
+  body.close_to_calving
+  ]
+  let queryText = ` INSERT INTO "animals" 
+    ("dam_id", "sire_id", "tag_number", "gender", "user_id", "birth_date", "disposition", "calving_ease", "castrated", "birthweight",  "calf", "close_to_calving" )
+    VALUES ($1,$2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *`
+  pool.query(queryText, values).then(result => {
+    res.send({ animal_id: result.rows[0].animal_id });
+    console.log(result.rows[0].animal_id)
+  }).catch(err => {
+    console.log('error posting cow', err);
+    res.sendStatus(500);
+  })
+});//end post route to /api/animal/cow
+
+
+//post to /api/animal/calf this is used to post a new calf to the database
+router.post('/calf', (req, res) => {
+  // console.log(req.body);
+  console.log(req.user);
+  console.log('adding a cow with tag number', req.body.tag_number);
+  let body = req.body;
+  let values = [
+    body.dam_id,
+    body.sire_id,
+    body.tag_number,
+    body.gender,
+    req.user.id,
+    body.birth_date,
+    body.calving_ease,
+    body.castrated,
+    body.birthweight,
+    body.calf,
+    body.close_to_calving
+  ];
+  let queryText = ` INSERT INTO "animals" 
+    ("dam_id", "sire_id", "tag_number", "gender", "user_id", "birth_date", "calving_ease", "castrated", "birthweight",  "calf", "close_to_calving" )
+    VALUES ($1,$2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *`
+  pool.query(queryText, values).then(result => {
+    res.send({ animal_id: result.rows[0].animal_id });
+    console.log(result.rows[0].animal_id)
+  }).catch(err => {
+    console.log('error posting cow', err);
+    res.sendStatus(500);
+  })
+});//end post route to /api/animal/cow
+
+module.exports = router;
