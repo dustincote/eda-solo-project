ac9205549213a0caa270ab3ef74e898643fab60f
Dustin Cote <dustincote@hotmail.com>
2020-10-31 03:18:38 -0500

    update pasture table itemm

--- src/components/Pasture/PastureTableItem.js
+++ src/components/Pasture/PastureTableItem.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 import { withRouter } from 'react-router';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+
 import moment from 'moment';
 
 
@@ -13,8 +12,7 @@ import moment from 'moment';
 
 
 
-//this is the component that will display all of the movies
-//we will dispatch to get movies and genres on loading of the component
+
 const PastureTableItem = (props) => {
     const row = props.row
 
